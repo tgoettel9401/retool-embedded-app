5b13dc6f632eb766957a39a3974304b227e9312d
Tobias <tgoettel9401@gmail.com>
2023-06-10 20:30:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Retool from "react-retool";
 import './App.css';
 
 function App() {
@@ -17,6 +18,14 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1>Embedding Retool-App now</h1>
+
+          <Retool
+              url="http://192.168.94.34:3000/embedded/public/2b872099-2947-4c45-a51c-cabfb9dbadc7">
+              data={{name: 'Tobias'}}
+          </Retool>
+
       </header>
     </div>
   );
